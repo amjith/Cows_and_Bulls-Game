91992627cafe4499baa80823a9d519026409069d
Amjith Ramanujam <amjith.r@gmail.com>
2010-11-13 22:10:03 -0700

    Adding the new unqiue words file.

--- cab.py
+++ cab.py
@@ -35,22 +35,48 @@ def check_chars(inp, actual):
     print "cow,bull", cow,bull
     return (cow,bull)
 
-def find_word():
-    f = open('words_4.txt','r')
+
+def has_dups(word):
+    """
+    >>> has_dups("")
+    0
+    >>> has_dups("abcdef")
+    0
+    >>> has_dups("aba")
+    1
+    >>> has_dups("abcdefc")
+    1
+    >>> has_dups("abcdeff")
+    1
+    """
+    if word == "":
+        return 0
+
+    for letter in word:
+        if word.count(letter) > 1:
+            return 1
+    return 0
+
+def pick_word():
+    """
+    Function to pick a word from the file
+    """
+    f = open('words_4_uniq.txt','r') # This file already has only unique words with no repeated characters
     words = f.read()
     f.close()
     words = words.splitlines()
-    return words[randint(0,len(words)-1)].lower()
+    uniq_words = [ word for word in words if has_dups(word) == 0 ]
+    return uniq_words[randint(0,len(uniq_words)-1)].lower()
 
 def main():
-    actual_word = find_word()
+    actual_word = pick_word()
     found = False
     attempts = 1
     print attempts, 
     inp_word = raw_input("Input a Word: ")
     inp_word = inp_word.lower()
     found = ((0,len(actual_word)) == check_chars(inp_word, actual_word))
-    while ( (not found) and  (attempts < len(actual_word) * 2)):
+    while ( (not found) and  (attempts < len(actual_word) * 3)):
         attempts += 1
         print attempts, 
         inp_word = raw_input("Input a Word: ")
@@ -59,8 +85,11 @@ def main():
     if found:
         print "Victory!!\nWow! that only took like %d attempts." %attempts
     else:
-        print "Fail!\nThe word was %s\nGo read a dictionary." %actual_word
+        print "Fail!\nThe word was %s\nGo read a dictionary." %actual_word.upper()
 
 
 if __name__=="__main__":
+    import doctest
+    doctest.testmod()
     main()
+
